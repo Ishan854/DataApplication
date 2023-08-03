4e37af63287a7ecbfb3f998bac1fb7efb979edee
Ishan854 <srivastava.ishan854@gmail.com>
2023-08-03 18:09:30 +0530

    Connected with postgresql

--- data_app_project/data_app_project/settings.py
+++ data_app_project/data_app_project/settings.py
@@ -17,7 +17,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 print(TEMPLATE_DIR)
 print(BASE_DIR)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -80,13 +79,25 @@ WSGI_APPLICATION = 'data_app_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+        
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST':'localhost',
+        'PORT':'5432'
         
     }
 }
+
 
 
 # Password validation
